381aede219d6caf1c976643d5b8a8b088f3bc36e
Abhisheknayak-programer <sahiservice300@gmail.com>
2025-07-12 16:02:12 +0530

    Deleted Duplicate Code Files

--- Practice-master/Practice-master/Arrays/MissingNumber/Better.js
+++
@@ -1,9 +0,0 @@
-const missingNumber = (nums) => {
-  let n = nums.length;
-  let expectedNums = (n * (n + 1)) / 2;
-  console.log(expectedNums);
-  const curSum = nums.reduce((acc, el) => (acc += el), 0);
-  return expectedNums - curSum;
-};
-
-console.log(missingNumber([0, 1, 2, 3, 5, 6]));
--- Practice-master/Practice-master/Arrays/MissingNumber/Brute.js
+++
@@ -1,8 +0,0 @@
-const missingNumber = (nums) => {
-  let n = nums.length;
-  for (let i = 0; i <= n; i++) {
-    if (!nums.includes(i)) return i;
-  }
-};
-
-console.log(missingNumber([0, 1, 2, 3, 5, 6]));
